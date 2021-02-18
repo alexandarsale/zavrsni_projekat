0eccab2c5d3c4228ed9551339352ad1394f89cea
Aleksandar Zafiroski <szafiroski@gmail.com>
2021-02-18 21:07:10 +0100

    Prvi

---
+++ src/zavrsni_projekat/Drugi.java
@@ -0,0 +1,10 @@
+package zavrsni_projekat;
+
+public class Drugi {
+
+	public static void main(String[] args) {
+		System.out.println("Proba Dva");
+
+	}
+
+}
---
+++ src/zavrsni_projekat/Prvi.java
@@ -0,0 +1,9 @@
+package zavrsni_projekat;
+
+public class Prvi {
+
+	public static void main(String[] args) {
+		System.out.println("Proba");
+	}
+
+}
